ede6a04f2e5faa4359ab82298ceed57f70bcc13f
overbejt <overbejt@miamioh.edu>
2018-02-24 17:35:44 -0500

    Changed the format settings in eclipse and added a seperate folder to hold the javadocs

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,31 +1,41 @@
+
+/**
+ * This is the main driver for the application
+ */
 package application;
-	
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
+import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			//Enter a print statement that says your name below here
-			System.out.println("Added by Josh");
-			//BorderPane root = new BorderPane();
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("View.fxml"));
+			// Enter a print statement that says
+			// your name below here
+			System.out.println(
+					"Added and formatted by Josh");
+			// BorderPane root = new BorderPane();
+			FXMLLoader loader = new FXMLLoader(
+					getClass().getResource(
+							"View.fxml"));
 			Parent root = loader.load();
-			Scene scene = new Scene(root,600,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Scene scene = new Scene(root, 600,
+					400);
+			scene.getStylesheets()
+					.add(getClass().getResource(
+							"application.css")
+							.toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
