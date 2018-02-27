705c5310723bcee9aef0d6fc60a23d2e483587db
unknown <>
2018-02-27 16:25:21 -0500

    Feiyu Wang

--- src/application/Main.java
+++ src/application/Main.java
@@ -21,7 +21,7 @@ public class Main extends Application {
 			System.out.println(
 					"Added and formatted by Josh");
 			System.out.println("Nicole Matthews");
-			
+			System.out.println("Feiyu Wang");
 			System.out.println("I Added another print statement");
 			System.out.println("Git push test");
 			
