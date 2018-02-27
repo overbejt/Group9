6257171ee92c301f6439f750433c59420698d18b
Josh Overbeck <overbejt@miamioh.edu>
2018-02-27 18:51:53 -0500

    Checking to see if this still works after merging

--- src/tests/EmployeeTest.java
+++ src/tests/EmployeeTest.java
@@ -1,5 +1,10 @@
 package tests;
 
+/**
+ * I made this class file while in another branch.  Then I merged the branch back to the master branch.
+ * @author Josh Overbeck
+ *
+ */
 public class EmployeeTest {
 
 	public static void main(String[] args) {
