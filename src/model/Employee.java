ec14462b2f15543acfb5c44ce63fad35fdb3abf0
overbejt <overbejt@miamioh.edu>
2018-03-02 02:24:23 -0500

    Implimented the EmployeeInterface in the Employee class.

--- src/model/Employee.java
+++ src/model/Employee.java
@@ -7,6 +7,55 @@ package model;
  * 
  * @version 0.1
  */
-public class Employee {
+public class Employee
+		implements EmployeeInterface {
+
+	@Override
+	public void setName(String name) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setPassword(String password) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setAccessLevel(int accessLevel) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setID() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getAccessLevel() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }// End of the 'Employee' class
