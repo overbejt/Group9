3b0b9cd127534b93cb9801205e3c050152da1730
overbejt <overbejt@miamioh.edu>
2018-03-03 00:56:00 -0500

    Created the Attributes class and updated javadocs

---
+++ src/model/Attributes.java
@@ -0,0 +1,79 @@
+package model;
+
+/**
+ * This is the class that will hold all of the
+ * attributes of an item.
+ * 
+ * @version 0.1
+ */
+public class Attributes
+		implements AttributesInterface {
+
+	// Instance variables
+	protected boolean shoes, pants, shirts;
+
+	public Attributes() {
+
+	}// End of the Constructor
+
+	/**
+	 * This is the method that allows a user to
+	 * assert that the item is shoes
+	 */
+	public void setShoes() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * This is the method that allows a user to
+	 * assert that the item is pants
+	 */
+	public void setPants() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * This is the method that allows a user to
+	 * assert that the item is a shirt
+	 */
+	public void setShirts() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * This is the method that will return true if
+	 * the item is asserted to be shoes
+	 * 
+	 * @return
+	 */
+	public boolean isShoes() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/**
+	 * This is the method that will return true if
+	 * the item is asserted to be pants
+	 * 
+	 * @return
+	 */
+	public boolean isPants() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/**
+	 * This is the method that will return true if
+	 * the item is assert to be a shirt
+	 * 
+	 * @return
+	 */
+	public boolean isShirts() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}// End of the 'Attributes' class
