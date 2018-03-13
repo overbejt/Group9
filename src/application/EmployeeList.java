86e9ea4dc888acf2eb101778d860aab9d98ccdee
Josh Overbeck <overbejt@miamioh.edu>
2018-03-12 23:53:42 -0400

    Finished the EmployeeList class

--- src/application/EmployeeList.java
+++ src/application/EmployeeList.java
@@ -19,13 +19,62 @@ public class EmployeeList implements Serializable {
 	private static final long serialVersionUID = 9162446095675838851L;
 
 	// Declaring instance variables
-	private Employee	employee;
-	private HashMap		list;
+	private Employee employee;
+
+	private HashMap<Long, Employee> list;
 
 	// Constructor
 	public EmployeeList() {
 		// Initializing the employee list
-		list = new HashMap();
+		list = new HashMap<Long, Employee>();
 	}// End of the Constructor
+
+	/**
+	 * This is the method that will add an employee to the employee
+	 * list.
+	 * 
+	 * @param employee
+	 */
+	public void addEmployee(Employee employee) {
+
+	}// End of the 'addEmployee' method
+
+	/**
+	 * This is the method that will remove an employee from the
+	 * employee list.
+	 * 
+	 * @param employee
+	 */
+	public void removeEmployee(Employee employee) {
+
+	}// End of the 'removeEmployee' method
+
+	/**
+	 * This is the method that will pass the reference to an Employee
+	 * in the Employee List.
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public Employee getEmployee(long id) {
+		return list.get(id);
+	}// End of the 'getEmployee' method
+
+	/**
+	 * This is the method that will return the Employee list in the
+	 * form of a HashMap
+	 * 
+	 * @return
+	 */
+	public HashMap getEmployeeList() {
+		return list;
+	}// End of the 'getEmployeeList' method
+
+	/**
+	 * This is the method that will clear the employee list.
+	 */
+	public void clearList() {
+		list.clear();
+	}// End of the 'clearList' method
 
 }// End of the 'EmployeeList' class
