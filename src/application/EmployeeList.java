3b07600909754e660d8285f4001d313e8f3d93ee
Josh Overbeck <overbejt@miamioh.edu>
2018-03-12 23:40:21 -0400

    Created the EmployeeList class

---
+++ src/application/EmployeeList.java
@@ -0,0 +1,18 @@
+package application;
+
+import java.io.Serializable;
+
+/**
+ * This is the class that will hold all of the employees. When an
+ * object of this class is initialized, it will be Serializable.
+ * 
+ * @version 1.0
+ */
+public class EmployeeList implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 9162446095675838851L;
+
+}// End of the 'EmployeeList' class
