c338b56e13ab8e1146fbcb8fbad1a05f7cfc3b77
overbejt <overbejt@miamioh.edu>
2018-03-31 16:36:26 -0400

    Fixed the focoused traversable issue on input fields

--- src/application/Main.java
+++ src/application/Main.java
@@ -26,10 +26,10 @@ public class Main extends Application {
 			// System.out.println("I Added another print statement");
 			// System.out.println("Git push test");
 
-			FXMLLoader loader = new FXMLLoader(getClass()
-					.getResource("/view/InventoryScene.fxml"));
-			// FXMLLoader loader = new FXMLLoader(
-			// getClass().getResource("/view/LoginScene.fxml"));
+			// FXMLLoader loader = new FXMLLoader(getClass()
+			// .getResource("/view/InventoryScene.fxml"));
+			FXMLLoader loader = new FXMLLoader(
+					getClass().getResource("/view/LoginScene.fxml"));
 
 			Parent root = loader.load();
 
