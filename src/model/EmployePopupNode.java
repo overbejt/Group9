f439d9e624f94c602a3ce905b9dad6f820634365
overbejt <overbejt@miamioh.edu>
2018-04-25 20:46:05 -0400

    Finished the default constructor

--- src/model/EmployePopupNode.java
+++ src/model/EmployePopupNode.java
@@ -1,13 +1,28 @@
 package model;
 
+import javafx.scene.control.RadioButton;
+
 /**
  * This is the class that will hold the input from the AddEmployee Pop
  * up.
  */
 public class EmployePopupNode {
+	// Declaring instance variables
+	private String		firstName;
+	private String		lastName;
+	private String		passwordA;
+	private String		passwordB;
+	private RadioButton	isEmployee;
+	private RadioButton	isManager;
 
-	public  EmployeePopupNode() {
-		
-	}
+	// Constructor
+	public EmployePopupNode() {
+		firstName = "";
+		lastName = "";
+		passwordA = "";
+		passwordB = "";
+		isEmployee = new RadioButton();
+		isManager = new RadioButton();
+	}// End of Constructor
 
 }// End of the 'EmployeePopupNode' class
