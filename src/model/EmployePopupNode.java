197d3d5ad1d50f3100b421e3cce27c76e654bbc6
overbejt <overbejt@miamioh.edu>
2018-04-25 20:42:03 -0400

    Generate a node class to hold the input from the add employee popup.

---
+++ src/model/EmployePopupNode.java
@@ -0,0 +1,13 @@
+package model;
+
+/**
+ * This is the class that will hold the input from the AddEmployee Pop
+ * up.
+ */
+public class EmployePopupNode {
+
+	public  EmployeePopupNode() {
+		
+	}
+
+}// End of the 'EmployeePopupNode' class
