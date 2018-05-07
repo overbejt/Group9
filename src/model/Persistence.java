187aefe0f6066c350f403ace61b5df10da1b44f2
Josh Overbeck <overbejt@miamioh.edu>
2018-05-07 13:26:09 -0400

    Commented the code

--- src/model/Persistence.java
+++ src/model/Persistence.java
@@ -147,28 +147,45 @@ public class Persistence {
 		// // Closing off the inputStream
 		// objectInputStream.close();
 
+		// Creating an instance of ItemList object
 		ItemList list = new ItemList();
+		/*
+		 * Creating an instance of a scanner and initializing it to
+		 * read from the ItemList file
+		 */
 		Scanner sc = new Scanner(itemList);
 
+		// Getting the data from the file
 		String data = sc.nextLine();
 		System.out.println("From the file: " + data);
 
+		// Closing the scanner to free up resources
 		sc.close();
 
+		// Splitting the data up into individual items as an array
 		String[] items = data.split("\\]");
 
+		// Loop through all the items in the list
 		for (String s : items) {
 
+			// Removing the delimiters
 			s = s.replaceAll("\\[", "");
 			s = s.replaceAll("\\]", "");
+			// Removing any excess white space
 			s = s.trim();
 
 			System.out.println("Item: " + s);
 
+			// Handling a special case from testing
 			if (s.startsWith(",")) {
 				s = s.substring(1);
 			}
 
+			/*
+			 * Splitting each item into an array as attributes. Index
+			 * 0 = id, Index 1 = name, Index 2 = price, Index 3 =
+			 * quantity, Index 4 = size
+			 */
 			String[] attributes = s.split("\\,");
 			// Clean up all attributes
 			// for (String a : attributes) {
@@ -180,26 +197,35 @@ public class Persistence {
 			// Handling the last null item
 			try {
 				attributes[0] = attributes[0].trim();
+				// Parse the id
 				long id = Long.parseLong(attributes[0]);
 				attributes[1] = attributes[1].trim();
+				// Parse the name
 				String name = attributes[1];
 				attributes[2] = attributes[2].trim();
+				// Parse the price
 				String price = attributes[2];
 				attributes[3] = attributes[3].trim();
+				// Parse the quantity
 				int quantity = Integer.parseInt(attributes[3]);
 				attributes[4] = attributes[4].trim();
+				// Parse the size
 				String size = attributes[4];
 
+				// Declaring a temp Item for adding to the list
 				Item item = new Item();
+				// Setting the attributes
 				item.setId(id);
 				item.setName(name);
 				item.setPrice(price);
 				item.setQuantity(quantity);
 				item.setSize(size);
 
+				// Adding the item to the list
 				list.addItem(item);
 
 				System.out.println("item done: " + item.toString());
+
 			} catch (Exception err) {
 				System.out.println(err);
 			}
